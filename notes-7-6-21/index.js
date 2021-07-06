f0d907cf891420acf3db7a767cc5daa1055312a1
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-07-06 14:13:30 -0500

    Added to notes-7-6-21

--- notes-7-6-21/index.js
+++ notes-7-6-21/index.js
@@ -182,3 +182,20 @@
 // https://www.hackerrank.com
 // This is a website that has lots of code challenges covering various topics
 // It specifically has a section to practice regex
+
+
+// For Next Time:
+// Given a specific year and a number representing the day of the year return 'Day {day} of {month name} in the year {year}'
+// Remember these rules about leap years:
+// if the year % 400 === 0 it is a leap year
+// if the year % 100 === 0 it is not a leap year (unless it is also divisible by 400)
+// if the year % 4 === 0 it is a leap year
+// If the year is a leap year February will have 29 days, if not it will have 28
+// Assume the year will always be AD
+
+// Tests
+// findDate(1979, 220) will return 'Day 8 of August in the year 1979'
+// findDate(1980, 220) will return 'Day 7 of August in the year 1980'
+// findDate(2000, 385) will return 'Day 19 of January in the year 2001'
+// findDate(800, 72) will return 'Day 12 of March in the year 800'
+// findDate(700, 72) will return 'Day 13 of March in the year 700'
